40a020f556c7db6868cba539528c9983da8d7dcb
ani-gencheva <an2000@abv.bg>
2014-11-12 17:44:24 +0200

    Revert "Revert "Revert "Revert "Defining-Classes-Homework""""
    This reverts commit eaf49a4339a0ce873be1881b1b3caff187b42f05.

---
+++ jumpstart-homework/jumpstart-homework/Laptop.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace jumpstart_homework
+{
+    class Laptop
+    {
+        private string model;
+        private decimal price;
+        private string manufacturer;
+        private string processor;
+        private string graphicsCard;
+        private int ram;
+        private Battery laptopBattery;
+
+        public Laptop(string model, decimal price)
+        {
+            this.Model = model;
+            this.Price = price;
+        }
+        public Laptop(string model, decimal price, string manufacturer, string processor, string graphicsCard,
+        int ram, Battery laptopBattery)
+            : this(model, price)
+        {
+            this.Manufacturer = manufacturer;
+            this.Processor = processor;
+            this.GraphicsCard = graphicsCard;
+            this.Ram = ram;
+            this.LaptopBattery = laptopBattery;
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Define the model of the laptop, please");
+                else
+                    this.model = value;
+            }
+        }
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value == 0.00M)
+                    throw new ArgumentOutOfRangeException("Choose Price,pleace ");
+                else
+                    this.price = value;
+            }
+        }
+        public string Manufacturer
+        {
+            get
+            {
+                return this.manufacturer;
+            }
+
+            set
+            {
+                if (value == " ")
+                    throw new ArgumentNullException("Choose the Manufacturer of the laptop, please");
+                else
+                    this.manufacturer = value;
+            }
+        }
+        public string Processor
+        {
+            get
+            {
+                return this.processor;
+            }
+            set
+            {
+                if (value == " ")
+                    throw new ArgumentNullException("Choose the Processor of the laptop, please");
+                else
+                    this.processor = value;
+            }
+        }
+        public int Ram
+        {
+            get { return this.ram; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentNullException("Ram can't be negative");
+                else
+                    this.ram = value;
+            }
+        }
+        public string GraphicsCard
+        {
+            get
+            {
+                return this.graphicsCard;
+            }
+            set
+            {
+                if (value == " ")
+                    throw new ArgumentNullException("Choose the grafics card  of the laptop, please");
+                else
+                    this.graphicsCard = value;
+            }
+        }
+        public Battery LaptopBattery
+        {
+            get { return this.laptopBattery; }
+            set { this.laptopBattery = value; }
+        }
+
+        public override string ToString()
+        {
+            string emptyField = "this field is empty";
+            return string.Format("Laptop( Model: {0}, Price: {1}, Manufacturer: {2}, Processor: {3}, Graphics card: {4}, RAM: {5} GB, Battery: {6}",
+                this.Model, this.Price.ToString("C2",CultureInfo.GetCultureInfo("bg-BG")),this.Manufacturer == null ? emptyField : this.Manufacturer,
+            this.Processor == null ? emptyField : this.Processor, this.GraphicsCard == null ? emptyField : this.GraphicsCard,
+            this.Ram == 0 ? emptyField : this.Ram.ToString(), this.LaptopBattery == null ? emptyField : LaptopBattery.ToString());
+        }
+    }
+}
